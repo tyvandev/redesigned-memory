7a28f1303ff57e5dc289bd86596eb17f06b2a352
andela-ofamamkinde <olatoyosi.famakinde@andela.co>
2023-04-19 02:39:13 -0400

    Add unique constraint on models and refactor views

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,46 @@
+import datetime
+
 from django.db import models
+
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('question_text').desc(),
+                name='question_text_unique',
+                violation_error_message='Question text must be unique'
+            )
+        ]
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('choice_text').desc(),
+                'question',
+                name='question_choice_text_unique',
+                violation_error_message='Choice text must be unique for a question'
+            )
+        ]
